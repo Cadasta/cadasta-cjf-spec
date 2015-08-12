299d2de35546850dbea017761ed2fc58746adc3c
Ryan Whitley <apollolm@gmail.com>
2015-08-12 15:41:37 -0700

    Added version
    Add version to data spec

--- 1.0/cjf-data.js
+++ 1.0/cjf-data.js
@@ -1,4 +1,5 @@
 {
+  version: 1.0,
   cadasta_id: "123456", //cadasta/CKAN user ID
   operation: "create", //create, update, delete
   survey_id: 125582, //The internal survey ID - survey must have been loaded into the system first
